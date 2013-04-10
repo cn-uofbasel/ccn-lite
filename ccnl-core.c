59218244e3690c5a658f6b24c347b492f64d01f9
tschudin <christian.tschudin@unibas.ch>
2013-04-10 17:50:56 +0200

    correct (signed char) for filtering out %C1 messages

--- ccnl-core.c
+++ ccnl-core.c
@@ -925,7 +925,7 @@ ccnl_core_RX(struct ccnl_relay_s *relay, int ifndx,
     if (ccnl_is_interest(buf)) {
 	DEBUGMSG(6, "  interest for <%s>\n", ccnl_prefix_to_path(prefix));
 	ccnl_print_stats(relay, STAT_RCV_I); //log count recv_interest
-	if (prefix->compcnt >= 1 && prefix->comp[0][0] == '\xC1') goto Done;
+	if (prefix->compcnt >= 1 && prefix->comp[0][0] == (unsigned char) 0xc1) goto Done;
 	if (prefix->compcnt == 4 && !memcmp(prefix->comp[0], "ccnx", 4)) {
 	    rc = ccnl_mgmt(relay, buf, prefix, from);
 	    goto Done;
