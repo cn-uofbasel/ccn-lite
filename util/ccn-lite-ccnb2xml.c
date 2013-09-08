da7cc76d5d08852f702da5383018221bc96ca807
Christopher Scherb <christopher.scherb@unibas.ch>
2013-09-08 14:39:53 +0200

    Remove code dublication in ccn-lite-deF and ccn-lite-mkF ccnb2xml verifies now signature from ccn-ext-mgmt reply

--- util/ccn-lite-ccnb2xml.c
+++ util/ccn-lite-ccnb2xml.c
@@ -34,6 +34,78 @@
 
 #include "ccnl-common.c"
 
+#include <openssl/sha.h>
+#include <openssl/rsa.h>
+#include <openssl/objects.h>
+#include <openssl/err.h>
+
+char *ctrl_public_key = "/home/blacksheeep/.ssh/publickey.pem";
+
+int sha1(void* input, unsigned long length, unsigned char* md)
+{
+    SHA_CTX context;
+    if(!SHA1_Init(&context))
+        return 0;
+
+    if(!SHA1_Update(&context, (unsigned char*)input, length))
+        return 0;
+
+    if(!SHA1_Final(md, &context))
+        return 0;
+
+    return 1;
+}
+
+int sign(char* private_key_path, char *msg, int msg_len, char *sig, int *sig_len)
+{
+
+    //Load private key
+    FILE *fp = fopen(private_key_path, "r");
+    if(!fp) {
+        printf("Could not find private key\n");
+        return 0;
+    }
+    RSA *rsa = (RSA *) PEM_read_RSAPrivateKey(fp,NULL,NULL,NULL);
+    fclose(fp);
+    if(!rsa) return 0;
+    
+    unsigned char md[SHA_DIGEST_LENGTH];
+    sha1(msg, msg_len, md);
+    
+    //Compute signatur
+    int err = RSA_sign(NID_sha1, md, SHA_DIGEST_LENGTH, sig, sig_len, rsa);
+    if(!err){
+        printf("Error: %d\n", ERR_get_error());
+    }
+    RSA_free(rsa);
+    return err;
+}
+
+int verify(char* public_key_path, char *msg, int msg_len, char *sig, int sig_len)
+{
+    //Load public key
+    FILE *fp = fopen(public_key_path, "r");
+    if(!fp) {
+        printf("Could not find public key\n");
+        return 0;
+    }
+    RSA *rsa = (RSA *) PEM_read_RSA_PUBKEY(fp, NULL, NULL, NULL);
+    if(!rsa) return 0;
+    fclose(fp);
+  
+    //Compute Hash
+    unsigned char md[SHA_DIGEST_LENGTH];
+    sha1(msg, msg_len, md);
+    
+    //Verify signature
+    int verified = RSA_verify(NID_sha1, md, SHA_DIGEST_LENGTH, sig, sig_len, rsa);
+    if(!verified){
+        //printf("Error: %d\n", ERR_get_error());
+    }
+    RSA_free(rsa);
+    return verified;
+}
+
 const char *byte_to_binary(int x)
 {
     static char b[9];
@@ -60,9 +132,9 @@ print_tag_content(unsigned char **buf, int *len, FILE *stream){
     while((**buf) !=  0)
     {
         printf("%c", **buf);
-        ++(*buf); ++(*len);
+        ++(*buf); --(*len);
     }
-    ++(*buf); ++(*len);
+    ++(*buf); --(*len);
     return 0;
 }
 
@@ -230,15 +302,6 @@ handle_ccn_debugreply_content(unsigned char **buf, int *len, int offset, char* t
                print_offset(offset+4); 
                print_tag_content_with_tag(buf, len, "PREFIX", stream);
                break;
-            case CCN_DTAG_SIGNATUREBITS:
-               print_offset(offset+4); 
-               //verify instead of print?
-               print_tag_content_with_tag(buf, len, "SIGNATUREBITS", stream);
-               break;
-            case CCN_DTAG_WITNESS:
-               print_offset(offset+4); 
-               print_tag_content_with_tag(buf, len, "WITNESS", stream);
-               break;
             default: 
               goto Bail;
         }
@@ -332,6 +395,47 @@ handle_ccn_debugrequest(unsigned char **buf, int *len, int offset, FILE *stream)
     return 0;
 }
 
+int 
+handle_ccn_signature(unsigned char **buf, int *buflen, int offset, FILE *stream)
+{
+   int num, typ, verified;
+   char *sigtype = 0, *sig = 0; 
+   while (dehead(buf, buflen, &num, &typ) == 0) {
+        
+        if (num==0 && typ==0)
+	    break; // end
+        
+        extractStr2(sigtype, CCN_DTAG_NAME);
+        extractStr2(sig, CCN_DTAG_SIGNATUREBITS);
+        
+        if (consume(typ, num, buf, buflen, 0, 0) < 0) goto Bail;
+    }
+    
+    char *buf2 = *buf;
+    int buflen2 = *buflen;
+    
+    if (dehead(&buf2, &buflen2, &num, &typ) != 0) goto Bail;
+    if (typ != CCN_TT_DTAG || num != CCN_DTAG_CONTENT) goto Bail;
+    
+    if (dehead(&buf2, &buflen2, &num, &typ) != 0) goto Bail;
+    if (typ != CCN_TT_BLOB) goto Bail;
+   
+    verified = verify(ctrl_public_key, buf2, buflen2 - 5, sig, 256);
+        
+    print_offset(offset); 
+    printf("<SIGNATURE>");
+    if(!verified) {
+        printf("signature NOT verified");
+    }else
+    {
+        printf("signature verified");
+    }
+    printf("</SIGNATURE>\n");
+    Bail:
+    
+    return 0;
+}
+
 int
 handle_ccn_content_obj(unsigned char **buf, int *len, int offset, FILE *stream)
 {
@@ -346,7 +450,7 @@ handle_ccn_content_obj(unsigned char **buf, int *len, int offset, FILE *stream)
                 handle_ccn_content(buf, len, offset+4, stream);
                 break;
             case CCN_DTAG_SIGNATURE: 
-                handle_ccn_debugreply_content(buf, len, offset+4, "SIGNATURE", stream);
+                handle_ccn_signature(buf, len, offset+4, stream);
                 break;
             default:
                 goto Bail;
--- util/ccnl-common.c
+++ util/ccnl-common.c
@@ -22,6 +22,100 @@
 
 // ----------------------------------------------------------------------
 
+
+#  define ccnl_malloc(s)	debug_malloc(s, __FILE__, __LINE__,timestamp())
+#  define ccnl_free(p)		debug_free(p, __FILE__, __LINE__)
+#  define CCNL_NOW()		current_time()
+
+struct mhdr {
+    struct mhdr *next;
+    char *fname;
+    int lineno, size;
+    char *tstamp;
+} *mem;
+
+char*
+timestamp(void)
+{
+    static char ts[30], *cp;
+
+    sprintf(ts, "%.4g", CCNL_NOW());
+    cp = strchr(ts, '.');
+    if (!cp)
+	strcat(ts, ".0000");
+    else if (strlen(cp) > 5)
+	cp[5] = '\0';
+    else while (strlen(cp) < 5)
+	strcat(cp, "0");
+    return ts;
+}
+
+void
+ccnl_get_timeval(struct timeval *tv)
+{
+    gettimeofday(tv, NULL);
+}
+
+int
+current_time(void)
+{
+    struct timeval tv;
+
+    ccnl_get_timeval(&tv);
+    return tv.tv_sec;
+}
+
+void*
+debug_malloc(int s, const char *fn, int lno, char *tstamp)
+{
+    struct mhdr *h = (struct mhdr *) malloc(s + sizeof(struct mhdr));
+    if (!h) return NULL;
+    h->next = mem;
+    mem = h;
+    h->fname = (char *) fn;
+    h->lineno = lno;
+    h->size = s;
+    h->tstamp = strdup(tstamp);
+    return ((unsigned char *)h) + sizeof(struct mhdr);
+}
+
+int
+debug_unlink(struct mhdr *hdr)
+{
+    struct mhdr **pp = &mem;
+
+    for (pp = &mem; pp; pp = &((*pp)->next)) {
+	if (*pp == hdr) {
+	    *pp = hdr->next;
+	    return 0;
+	}
+	if (!(*pp)->next)
+	    break;
+    }
+    return 1;
+}
+
+void
+debug_free(void *p, const char *fn, int lno)
+{
+    struct mhdr *h = (struct mhdr *) (((unsigned char *)p) - sizeof(struct mhdr));
+
+    if (!p) {
+//	fprintf(stderr, "%s: @@@ memerror - free() of NULL ptr at %s:%d\n",
+//	   timestamp(), fn, lno);
+	return;
+    }
+    if (debug_unlink(h)) {
+	fprintf(stderr,
+	   "%s: @@@ memerror - free() at %s:%d does not find memory block %p\n",
+		timestamp(), fn, lno, p);
+	return;
+    }
+    if (h->tstamp)
+	free(h->tstamp);
+    free(h);
+}
+
 static int consume(int typ, int num, unsigned char **buf, int *len,
 		   unsigned char **valptr, int *vallen);
 
@@ -80,5 +174,27 @@ consume(int typ, int num, unsigned char **buf, int *len,
 //  case DTAG, DATTR:
     return -1;
 }
+
+#define extractStr(VAR,DTAG) \
+    if (typ == CCN_TT_DTAG && num == DTAG) { \
+	char *s; unsigned char *valptr; int vallen; \
+	if (consume(typ, num, &buf, &buflen, &valptr, &vallen) < 0) goto Bail; \
+	s = ccnl_malloc(vallen+1); if (!s) goto Bail; \
+	memcpy(s, valptr, vallen); s[vallen] = '\0'; \
+	ccnl_free(VAR); \
+	VAR = (unsigned char*) s; \
+	continue; \
+    } do {} while(0)
+
+#define extractStr2(VAR,DTAG) \
+    if (typ == CCN_TT_DTAG && num == DTAG) { \
+	char *s; unsigned char *valptr; int vallen; \
+	if (consume(typ, num, buf, buflen, &valptr, &vallen) < 0) goto Bail; \
+	s = ccnl_malloc(vallen+1); if (!s) goto Bail; \
+	memcpy(s, valptr, vallen); s[vallen] = '\0'; \
+	ccnl_free(VAR); \
+	VAR = (unsigned char*) s; \
+	continue; \
+    } do {} while(0)
 
 // eof
