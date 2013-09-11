862ac3e70dbdd8c8322ef7abc9d915c059b9ee57
Christopher Scherb <christopher.scherb@unibas.ch>
2013-09-11 14:22:53 +0200

    Merge the tool ccn-lite-put to ccn-lite-ctrl

--- util/ccn-lite-ctrl.c
+++ util/ccn-lite-ctrl.c
@@ -30,6 +30,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <sys/stat.h>
 
 #include "../ccnx.h"
 #include "../ccnl.h"
@@ -37,7 +38,158 @@
 #include "ccnl-common.c"
 #include "../ccnl-pdu.c"
 
+#ifdef CCNL_USE_MGMT_SIGNATUES
+#include <openssl/sha.h>
+#include <openssl/rsa.h>
+#include <openssl/objects.h>
+#include <openssl/err.h>
+#endif /*CCNL_USE_MGMT_SIGNATUES*/
+
 // ----------------------------------------------------------------------
+
+
+#ifdef CCNL_USE_MGMT_SIGNATUES
+int sha1(void* input, unsigned long length, unsigned char* md)
+{
+    SHA_CTX context;
+    if(!SHA1_Init(&context))
+        return 0;
+
+    if(!SHA1_Update(&context, (unsigned char*)input, length))
+        return 0;
+
+    if(!SHA1_Final(md, &context))
+        return 0;
+
+    return 1;
+}
+
+int sign(char* private_key_path, char *msg, int msg_len, char *sig, int *sig_len)
+{
+
+    //Load private key
+    FILE *fp = fopen(private_key_path, "r");
+    if(!fp) {
+        printf("Could not find private key\n");
+        return 0;
+    }
+    RSA *rsa = (RSA *) PEM_read_RSAPrivateKey(fp,NULL,NULL,NULL);
+    fclose(fp);
+    if(!rsa) return 0;
+    
+    unsigned char md[SHA_DIGEST_LENGTH];
+    sha1(msg, msg_len, md);
+    
+    //Compute signatur
+    int err = RSA_sign(NID_sha1, md, SHA_DIGEST_LENGTH, sig, sig_len, rsa);
+    if(!err){
+        printf("Error: %d\n", ERR_get_error());
+    }
+    RSA_free(rsa);
+    return err;
+}
+
+int verify(char* public_key_path, char *msg, int msg_len, char *sig, int sig_len)
+{
+    //Load public key
+    FILE *fp = fopen(public_key_path, "r");
+    if(!fp) {
+        printf("Could not find public key\n");
+        return 0;
+    }
+    RSA *rsa = (RSA *) PEM_read_RSA_PUBKEY(fp, NULL, NULL, NULL);
+    if(!rsa) return 0;
+    fclose(fp);
+    
+    //Compute Hash
+    unsigned char md[SHA_DIGEST_LENGTH];
+    sha1(msg, msg_len, md);
+    
+    //Verify signature
+    int verified = RSA_verify(NID_sha1, md, SHA_DIGEST_LENGTH, sig, sig_len, rsa);
+    if(!verified){
+        printf("Error: %d\n", ERR_get_error());
+    }
+    RSA_free(rsa);
+    return verified;
+}
+
+int add_signature(unsigned char *out, char *private_key_path, char *file, int fsize)
+{
+    int len, i;
+    
+    unsigned char sig[2048];
+    int sig_len;
+
+    len = mkHeader(out, CCN_DTAG_SIGNATURE, CCN_TT_DTAG);
+    len += mkStrBlob(out + len, CCN_DTAG_NAME, CCN_TT_DTAG, "SHA1");
+    len += mkStrBlob(out + len, CCN_DTAG_WITNESS, CCN_TT_DTAG, "");
+    
+    if(!sign(private_key_path, file, fsize, sig, &sig_len)) return 0;
+    printf("SIGLEN: %d\n",sig_len);
+    sig[sig_len]=0;
+    
+    //add signaturebits bits...
+    len += mkHeader(out + len, CCN_DTAG_SIGNATUREBITS, CCN_TT_DTAG);
+    len += addBlob(out + len, sig, sig_len);
+    out[len++] = 0; // end signaturebits
+    
+    out[len++] = 0; // end signature
+    
+    /*char *publickey = "/home/blacksheeep/.ssh/publickey.pem";
+    int verified = verify(publickey, file, fsize, sig, sig_len);
+    printf("Verified: %d\n", verified);*/
+    
+    return len;
+}
+#endif /*CCNL_USE_MGMT_SIGNATUES*/
+
+int
+split_string(char *in, char c, char *out)
+{
+    
+    int i = 0, j = 0;
+    if(!in[0]) return 0;
+    if(in[0] == c) ++i;
+    while(in[i] != c)
+    {
+        if(in[i] == 0) break;
+        out[j] = in[i];
+        ++i; ++j;
+        
+    }
+    out[j] = 0;
+    return i;  
+}
+
+time_t
+get_mtime(const char *path)
+{
+    struct stat statbuf;
+    if (stat(path, &statbuf) == -1) {
+        perror(path);
+        exit(1);
+    }
+    return statbuf.st_mtime;
+}
+
+int
+add_ccnl_name(unsigned char *out, char *ccn_path)
+{
+    char comp[256];
+    int len = 0, len2 = 0;
+    int h;
+    memset(comp, 0 , 256);
+    len += mkHeader(out + len, CCN_DTAG_NAME, CCN_TT_DTAG);
+    while( (h = split_string(ccn_path + len2, '/', comp)) )
+    {   
+        len2 += h;
+        len += mkStrBlob(out + len, CCN_DTAG_COMPONENT, CCN_TT_DTAG, comp);
+        memset(comp, 0 , 256);
+    }
+    out[len++] = 0;
+    return len;
+}
 
 int
 mkDebugRequest(unsigned char *out, char *dbg)
@@ -402,6 +554,160 @@ mkPrefixregRequest(unsigned char *out, char reg, char *path, char *faceid)
     return len;
 }
 
+int
+createCCNXFile(char *file_uri, char *ccn_path, char *private_key_path)
+{
+    long fsize, len, siglen;
+    char *out, *file, *new_file_uri;
+    FILE *f = fopen(file_uri, "r");
+    if(!f) return 0;
+    
+    //size vom file abmessen
+    fseek(f, 0L, SEEK_END);
+    fsize = ftell(f);
+    fseek(f, 0L, SEEK_SET);
+    
+    file = (unsigned char * ) malloc(sizeof(unsigned char)*fsize);
+    fread(file, fsize, 1, f);
+    fclose(f);
+    out = (unsigned char *) malloc(sizeof(unsigned char)*fsize + 1000);
+
+    
+    len = mkHeader(out, CCN_DTAG_CONTENTOBJ, CCN_TT_DTAG); //content object
+    
+#ifdef CCNL_USE_MGMT_SIGNATUES
+    siglen = add_signature(out + len, private_key_path, file, fsize);
+    if(!siglen)
+    {
+        printf("Could sign message\n");
+    }
+    len += siglen;
+#endif /*CCNL_USE_MGMT_SIGNATUES*/
+    
+    len += add_ccnl_name(out + len, ccn_path);
+    
+    len += mkStrBlob(out + len, CCN_DTAG_CONTENT, CCN_TT_DTAG, (char *) file);
+
+    out[len++] = 0; //content object end
+    
+    //write file
+    new_file_uri = (char *) malloc(sizeof(char)*1024);
+    sprintf(new_file_uri, "%s.ccnb", file_uri);
+    FILE *f2 = fopen(new_file_uri, "w");
+    if(!f2) return 0;
+    fwrite(out, 1L, len, f2);
+    fclose(f2);
+    free(out);
+    free(file);
+    
+    return 1;
+}
+
+int
+mkAddToRelayCacheRequest(unsigned char *out, char *file_uri, char *private_key_path)
+{
+    long len = 0, len2 = 0, len3 = 0;
+    long fsize;
+    char *ccnb_file, *contentobj, *stmt;
+    FILE *f = fopen(file_uri, "r");
+    if(!f) return 0;
+    
+    //determine size of the file
+    fseek(f, 0L, SEEK_END);
+    fsize = ftell(f);
+    fseek(f, 0L, SEEK_SET);
+    
+    ccnb_file = (unsigned char *) malloc(sizeof(unsigned char)*fsize);
+    fread(ccnb_file, fsize, 1, f);
+    fclose(f);
+   
+    //Create ccn-lite-ctrl interest object with signature to add content...
+    //out = (unsigned char *) malloc(sizeof(unsigned char)*fsize + 5000);
+    contentobj = (unsigned char *) malloc(sizeof(unsigned char)*fsize + 4000);
+    stmt = (unsigned char *) malloc(sizeof(unsigned char)*fsize + 1000);
+    
+    len = mkHeader(out, CCN_DTAG_INTEREST, CCN_TT_DTAG);   // interest
+    len += mkHeader(out+len, CCN_DTAG_NAME, CCN_TT_DTAG);  // name
+
+    len += mkStrBlob(out+len, CCN_DTAG_COMPONENT, CCN_TT_DTAG, "ccnx");
+    len += mkStrBlob(out+len, CCN_DTAG_COMPONENT, CCN_TT_DTAG, "");
+    len += mkStrBlob(out+len, CCN_DTAG_COMPONENT, CCN_TT_DTAG, "addcacheobject");
+    
+    //add content to interest...
+    len3 += mkHeader(stmt+len3, CCN_DTAG_CONTENT, CCN_TT_DTAG);
+    len3 += addBlob(stmt+len3, ccnb_file, fsize);
+    stmt[len3++] = 0; // end content
+    
+    len2 += mkHeader(contentobj+len2, CCN_DTAG_CONTENTOBJ, CCN_TT_DTAG);   // contentobj
+#ifdef CCNL_USE_MGMT_SIGNATUES
+    if(private_key_path) len2 += add_signature(contentobj+len2, private_key_path, stmt, len3);
+#endif /*CCNL_USE_MGMT_SIGNATUES*/
+ 
+    len2 += mkBlob(contentobj+len2, CCN_DTAG_CONTENT, CCN_TT_DTAG,  // content
+		   (char*) stmt, len3);
+    contentobj[len2++] = 0; // end-of-contentobj
+    
+    
+    len += mkBlob(out+len, CCN_DTAG_COMPONENT, CCN_TT_DTAG,  // comp
+		  (char*) contentobj, len2);
+    
+    out[len++] = 0; //name end
+    out[len++] = 0; //interest end
+    
+    Bail:
+    free(ccnb_file);
+    free(contentobj);
+    free(stmt);
+    return len;
+}
+
+int
+mkRemoveFormRelayCacheRequest(unsigned char *out, char *ccn_path, char *private_key_path){
+    
+    int len = 0, len2 = 0, len3 = 0;
+    
+    unsigned char contentobj[2000];
+    unsigned char stmt[1000];
+
+    len = mkHeader(out, CCN_DTAG_INTEREST, CCN_TT_DTAG);   // interest
+    len += mkHeader(out+len, CCN_DTAG_NAME, CCN_TT_DTAG);  // name
+
+    len += mkStrBlob(out+len, CCN_DTAG_COMPONENT, CCN_TT_DTAG, "ccnx");
+    len += mkStrBlob(out+len, CCN_DTAG_COMPONENT, CCN_TT_DTAG, "");
+    //signatur nach hier, über den rest
+    len += mkStrBlob(out+len, CCN_DTAG_COMPONENT, CCN_TT_DTAG, "removecacheobject");
+
+    // prepare debug statement
+    //len3 = mkHeader(stmt, CCNL_DTAG_DEBUGREQUEST, CCN_TT_DTAG);
+    len3 += add_ccnl_name(stmt+len3, ccn_path);
+    
+    //stmt[len3++] = 0; // end-of-debugstmt
+
+    // prepare CONTENTOBJ with CONTENT
+    len2 = mkHeader(contentobj, CCN_DTAG_CONTENTOBJ, CCN_TT_DTAG);   // contentobj
+    
+#ifdef CCNL_USE_MGMT_SIGNATUES
+    if(private_key_path)len2 += add_signature(contentobj+len2, private_key_path, stmt, len3);
+#endif 
+    len2 += mkBlob(contentobj+len2, CCN_DTAG_CONTENT, CCN_TT_DTAG,  // content
+		   (char*) stmt, len3);
+    contentobj[len2++] = 0; // end-of-contentobj
+
+    
+    
+    // add CONTENTOBJ as the final name component
+    len += mkBlob(out+len, CCN_DTAG_COMPONENT, CCN_TT_DTAG,  // comp
+		  (char*) contentobj, len2);
+
+    out[len++] = 0; // end-of-name
+    out[len++] = 0; // end-of-interest
+
+//    ccnl_prefix_free(p);
+    
+    return len;
+ 
+}
+
 // ----------------------------------------------------------------------
 
 int
@@ -460,7 +766,12 @@ main(int argc, char *argv[])
     unsigned char out[64000];
     int len;
     int sock = 0;
-
+    
+    char *unix_socket_path;
+    char *file_uri;
+    char *ccn_path;
+    char *private_key_path = 0;
+    
     if (argv[1] && !strcmp(argv[1], "-x") && argc > 2) {
 	ux = argv[2];
 	argv += 2;
@@ -518,7 +829,25 @@ main(int argc, char *argv[])
     } else if (!strcmp(argv[1], "prefixunreg")) {
 	if (argc < 4) goto Usage;
 	len = mkPrefixregRequest(out, 0, argv[2], argv[3]);
-    } else {
+    } else if (!strcmp(argv[1], "add")){
+        if(argc < 3) goto Usage;
+        file_uri = argv[2];  
+        if(argc > 3)private_key_path = argv[3];
+        len = mkAddToRelayCacheRequest(out, file_uri, private_key_path);
+    } else if(!strcmp(argv[1], "remove")){
+        if(argc < 3) goto Usage;
+        ccn_path = argv[2];  
+        if(argc > 3)private_key_path = argv[3];
+        len = mkRemoveFormRelayCacheRequest(out, ccn_path, private_key_path);
+    } else if(!strcmp(argv[1], "create"))
+    {
+        if(argc < 5) goto Usage;
+        file_uri = argv[2];
+        ccn_path = argv[3];
+        private_key_path = argv[4];
+        if(!createCCNXFile(file_uri, ccn_path, private_key_path)) goto Usage;
+        return 0;
+    } else{
 	printf("unknown command %s\n", argv[1]);
 	goto Usage;
     }
