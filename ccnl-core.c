e5888891835f4007ad770ea15215a58d84ecffe8
Christopher Scherb <christopher.scherb@unibas.ch>
2013-10-23 16:04:43 +0200

    Kernelmodule can now be installed.

--- ccnl-core.c
+++ ccnl-core.c
@@ -23,6 +23,11 @@
 
 #define CCNL_VERSION "2013-07-27"
 
+
+int
+ccnl_crypto(struct ccnl_relay_s *ccnl, struct ccnl_buf_s *orig,
+	  struct ccnl_prefix_s *prefix, struct ccnl_face_s *from);
+
 static struct ccnl_interest_s* ccnl_interest_remove(struct ccnl_relay_s *ccnl,
 						    struct ccnl_interest_s *i);
 
