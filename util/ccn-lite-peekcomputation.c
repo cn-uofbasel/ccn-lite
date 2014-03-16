bd504120f02401869b0664f90c22a5fc2ee974ac
Christopher Scherb <christopher.scherb@unibas.ch>
2014-03-16 01:27:09 +0100

    Update synchronous content computation handling

--- util/ccn-lite-peekcomputation.c
+++ util/ccn-lite-peekcomputation.c
@@ -1,9 +1,14 @@
+<<<<<<< HEAD
 #include <ctype.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+=======
+#include <stdlib.h>
+#include <stdio.h>
+>>>>>>> 77d83cd... Add utility to create compute requests
 
 #include <sys/types.h>
 #include <sys/select.h>
@@ -18,6 +23,7 @@
 #include "ccnl-common.c"
 #include "../krivine-common.c"
 
+<<<<<<< HEAD
 #include "ccnl-socket.c"
 
 
@@ -86,5 +92,24 @@ Usage:
 	sendproc = udp_sendto;
     }
     request_content(sock, sendproc, dest, out, len, wait);
+=======
+
+int main(int argc, char **argv){
+    
+    char out[CCNL_MAX_PACKET_SIZE];
+    
+    char *namecomp[CCNL_MAX_NAME_COMP];
+    
+    int i, j= splitComponents(argv[1], namecomp);
+    
+    //for(i = 0; i < j; ++i)
+        //printf("%s\n", namecomp[i]);
+    
+    int len = mkInterestCompute(namecomp, argv[2], strlen(argv[2]), 0, out);
+   // printf("len: %d\n", len);
+    
+    fwrite(out, sizeof(char), len, stdout);
+
+>>>>>>> 77d83cd... Add utility to create compute requests
     return 0;
 }
