ada4ced79681ab634db065dce30197b630eb2ed3
Basil Kohler <basil.kohler@gmail.com>
2014-03-18 12:29:02 +0100

    udp sender properly logs messages and improved ScalaToNFN implementation

--- nfn/ccnliteinterface/src/main/scala/UDPServer.scala
+++ nfn/ccnliteinterface/src/main/scala/UDPServer.scala
@@ -1,4 +1,6 @@
 import akka.actor.{ActorRef, ActorSystem, IOManager}
+import akka.io.UdpConnected
+import akka.io.UdpConnected.Connect
 import akka.util.ByteString
 import java.net.{DatagramPacket, DatagramSocket}
 
@@ -35,7 +37,7 @@ import java.net.InetSocketAddress
  * For improved performance implement a bound UDP sender, which
  * does not need to perform certain security checks with each call
  */
-class SimpleUDPSender(port: Int = 9000, host: String = "localhost") extends Actor {
+class UDPSender(nextActor: ActorRef, port: Int = 9000, host: String = "localhost") extends Actor {
   import context.system
 
   val remote = new InetSocketAddress(host, port)
@@ -57,27 +59,66 @@ class SimpleUDPSender(port: Int = 9000, host: String = "localhost") extends Acto
   }
 }
 
+case class Send(data: Array[Byte])
+
 /**
  * UDP server
 // * @param nextActor
  */
-class UDPListener(nextActor: ActorRef, port: Int = 9000, host: String = "localhost") extends Actor {
+class UDPConnection(nextActor: ActorRef, port: Int = 9000, host: String = "localhost") extends Actor {
   import context.system
+
+  val target = new InetSocketAddress(host, port)
   // IO is the manager of the akka IO layer, send it a request
   // to listen on a certain host on a port
-  IO(Udp) ! Udp.Bind(self, new InetSocketAddress(host, port))
+  IO(Udp) ! Udp.Bind(self, target)
 
   def receive = {
     // Received udp listener actor, set it as own actor
     case Udp.Bound(local) =>
       println("UDPListener becomes ready")
       context.become(ready(sender))
+    case Udp.CommandFailed(cmd) =>
+      println(s"Udp command '$cmd' failed")
   }
 
   def ready(socket: ActorRef): Receive = {
-    case Udp.Received(data, remote) => nextActor ! data
+    case Udp.Received(data, remote) => {
+      println(s"Received: $data")
+      nextActor ! data
+    }
     case Udp.Unbind  => socket ! Udp.Unbind
     case Udp.Unbound => context.stop(self)
+
+    case Send(data) =>
+      println(s"sending data: $data")
+      self ! Udp.Send(ByteString(data), target)
+  }
+
+  def send(data: Array[Byte]) = {
+  }
+}
+
+class Connected(hostname: String, port: Int) extends Actor {
+  import context.system
+
+  val remote = new InetSocketAddress(hostname, port)
+  IO(UdpConnected) ! UdpConnected.Connect(self, remote)
+
+  def receive = {
+    case UdpConnected.Connected =>
+      context.become(ready(sender))
+    case Udp.CommandFailed(cmd) =>
+      println(s"Udp command '$cmd' failed")
+  }
+
+  def ready(connection: ActorRef): Receive = {
+    case UdpConnected.Received(data) =>
+    // process data, send it on, etc.
+    case msg: String =>
+      connection ! UdpConnected.Send(ByteString(msg))
+    case d @ UdpConnected.Disconnect => connection ! d
+    case UdpConnected.Disconnected   => context.stop(self)
   }
 }
 
@@ -103,16 +144,15 @@ import akka.actor._
 object AkkaTest extends App {
   val system = ActorSystem("NFNActorSystem")
 
-  val worker = system.actorOf(Props[NFNWorker], name = "NFNWorker")
-  val udpListener = system.actorOf(Props(new UDPListener(worker)), name = "UDPListener")
+//  val worker = system.actorOf(Props[NFNWorker], name = "NFNWorker")
+//  val udpConnection = system.actorOf(Props(new UDPConnection(worker)), name = "UDPListener")
 
-  val simpleSender = system.actorOf(Props(new SimpleUDPSender), name = "SimpleUDPSender")
+//  val simpleSender = system.actorOf(Props(new SimpleUDPSender), name = "SimpleUDPSender")
+//  Thread.sleep(2000)
+//
+//  val interest = "add 1 1/NFN"
+//  udpConnection !  Send(new CCNLiteInterface().mkBinaryInterest(interest))
 
-  Thread.sleep(1000)
-
-  val interest = "add 1 1/NFN"
-  println(s"sending: $interest")
-  simpleSender ! new CCNLiteInterface().mkBinaryInterest(interest)
 }
 
 
