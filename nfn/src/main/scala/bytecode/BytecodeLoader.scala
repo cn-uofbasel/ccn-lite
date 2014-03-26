bb4fadaa2380c2e83a8699f85b7b6f97941d2275
Basil Kohler <basil.kohler@gmail.com>
2014-03-26 15:49:24 +0100

    When advertising services, all unpinned services add their own bytecode as data to the content object and added unixsocket library with native code

--- nfn/src/main/scala/bytecode/BytecodeLoader.scala
+++ nfn/src/main/scala/bytecode/BytecodeLoader.scala
@@ -3,10 +3,8 @@ package bytecode
 import java.net.URLClassLoader
 import scala.io.Source
 import java.io.File
+import java.nio.file.{Paths, Path, Files}
 
-/**
- * Created by basil on 24/02/14.
- */
 object BytecodeLoader {
   def loadClassFromJar[T](jarfile: String, className: String): T = {
     val urls = Array(new File(jarfile).toURL)
@@ -17,9 +15,21 @@ object BytecodeLoader {
 
   def jarToByteArray(jarfile: String): Array[Byte] = Source.fromFile(jarfile).getLines.mkString.getBytes
 
-  def getClassfileOfClass(clazz: Any):Option[File] = {
-    val classFile = new File(clazz.getClass.getProtectionDomain.getCodeSource.getLocation.getFile + this.getClass.getName + ".class")
+  def classfileOfClass(clazz: Any):Option[File] = {
+    def classToFilename(clazz: Any) = {
+      def classBaseDirectory =  clazz.getClass.getProtectionDomain.getCodeSource.getLocation.getFile
+      def classNamespaceDirectoriesAndName = clazz.getClass.getName.replace(".", "/")
+      classBaseDirectory + classNamespaceDirectoriesAndName + ".class"
+    }
+    val classFile = new File(classToFilename(clazz))
     if(!classFile.exists) None
     else Some(classFile)
+  }
+
+  def fromClass(clazz: Any):Option[Array[Byte]] = {
+    classfileOfClass(clazz) map { classFile =>
+      val path = Paths.get(classFile.toURI)
+      Files.readAllBytes(path)
+    }
   }
 }
