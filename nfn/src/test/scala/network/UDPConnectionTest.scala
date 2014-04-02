072fce3419cd4cebddfe63d45e87d1a1854cba70
Basil Kohler <basil.kohler@gmail.com>
2014-04-02 17:47:41 +0200

    First couple of tests for NFNMaster, not finished

--- nfn/src/test/scala/network/UDPConnectionTest.scala
+++ nfn/src/test/scala/network/UDPConnectionTest.scala
@@ -10,34 +10,38 @@ import org.scalatest.WordSpecLike
 import org.scalatest.Matchers
 import org.scalatest.BeforeAndAfterAll
 import akka.testkit.ImplicitSender
+import java.net.InetSocketAddress
+import network.UDPConnection.Send
 
-object MySpec {
-  class EchoActor extends Actor {
-    def receive = {
-      case x => sender ! x
-    }
-  }
-}
+//object MySpec {
+//  class EchoActor extends Actor {
+//    def receive = {
+//      case x => sender ! x
+//    }
+//  }
+//}
 
-class MySpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
-with WordSpecLike with Matchers with BeforeAndAfterAll {
-
-  def this() = this(ActorSystem("MySpec"))
-
-  import MySpec._
-
-  override def afterAll {
-    TestKit.shutdownActorSystem(system)
-  }
-
-  "An Echo actor" must {
-
-    "send back messages unchanged" in {
-      val echo = system.actorOf(Props[EchoActor])
-      echo ! "hello world"
-      expectMsg("hello world")
-    }
-
-  }
-}
+//class UDPConnectionSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
+//with WordSpecLike with Matchers with BeforeAndAfterAll {
+//
+//
+//  val sock1 = new InetSocketAddress(9010)
+//  val sock2 = new InetSocketAddress(9011)
+//  val data = "test".getBytes
+//  def this() = this(ActorSystem("UDPConnectionSpec"))
+//
+//  override def afterAll {
+//    TestKit.shutdownActorSystem(system)
+//  }
+//
+//  "An Echo actor" must {
+//
+//    "send back messages unchanged" in {
+//      val actualCon = system.actorOf(Props(new UDPConnection(local = sock1, target = sock2)))
+//      actualCon ! UDPConnection.Send(data)
+//      expectMsg(data)
+//    }
+//
+//  }
+//}
 
