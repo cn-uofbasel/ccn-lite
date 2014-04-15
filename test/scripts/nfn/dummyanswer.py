2d1779b73123ec53a4b4b7836e7e6a98590a2656
Christopher Scherb <christopher.scherb@unibas.ch>
2014-04-15 11:11:02 +0200

    Update for bsd

--- test/scripts/nfn/dummyanswer.py
+++ test/scripts/nfn/dummyanswer.py
@@ -7,6 +7,9 @@ UDP_IP = "127.0.0.1"
 UDP_PORT = 9002
 
 
+f_t = open("/home/blacksheeep/ccn-lite/test/ccnb/nfn/computation_dummy_thunk.ccnb")
+c_t = f_t.read()
+
 f = open("/home/blacksheeep/ccn-lite/test/ccnb/nfn/computation_dummy_result.ccnb")
 c = f.read()
 
@@ -14,9 +17,12 @@ sock = socket.socket(socket.AF_INET, # Internet
                   socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
-#while True:
-data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-print "received message:", data
-time.sleep(2)
-sock.sendto(c, ("127.0.0.1", 9001))
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print "received message:", data
+    time.sleep(2)
+    if "THUNK" in data:
+        sock.sendto(c_t, ("127.0.0.1", 9001))
+    else:
+        sock.sendto(c, ("127.0.0.1", 9001))
 
