79d49fb739012c5b2960754aca198edd7cb4e811
Christopher Scherb <christopher.scherb@unibas.ch>
2014-05-06 14:15:02 +0200

    Monitor messags log nano seconds instead of milli seconds

--- json.c
+++ json.c
@@ -2,8 +2,19 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <time.h>
 #include "ccnl-core.h"
+
+#include <time.h>
+#include <sys/time.h>
+
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
+
+
+
 
 #define CCNL_MAX_PACKET_SIZE 8192
 
@@ -129,10 +140,24 @@ create_packet_log(/*long fromip, int fromport,*/ char* toip, int toport,
     len += sprintf(res + len, "},\n"); //to
 
     len += sprintf(res + len, "\"isSent\": %s,\n", "true");
-    struct timeval  tv;
-    gettimeofday(&tv, NULL);
+
+    struct timespec ts;
+
+    #ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    ts.tv_sec = mts.tv_sec;
+    ts.tv_nsec = mts.tv_nsec;
+
+    #else
+    clock_gettime(CLOCK_REALTIME, &ts);
+    #endif
+
     long timestamp_milli =
-        ((tv.tv_sec) * 1000 + (tv.tv_usec / 1000)); // convert tv_sec & tv_usec to millisecond
+        ((ts.tv_sec) * 1000000000 + (ts.tv_nsec ));
     len += sprintf(res + len, "\"timestamp\": %lu,\n", timestamp_milli);
     
     len += sprintf(res + len, "\"packet\":{\n");
