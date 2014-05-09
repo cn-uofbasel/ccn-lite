5774d1f97e269abe8702f17028a6f3aaeb64eb44
Christopher Scherb <christopher.scherb@unibas.ch>
2014-05-09 14:34:23 +0200

    Change local content search, refactoring

--- krivine-common.c
+++ krivine-common.c
@@ -109,8 +109,8 @@ splitComponents(char* namecomp, char **prefix)
     cp = strtok(namecomp, "/");
     
     while (i < (CCNL_MAX_NAME_COMP - 1) && cp) {
-	prefix[i++] = cp;
-	cp = strtok(NULL, "/");
+        prefix[i++] = cp;
+        cp = strtok(NULL, "/");
     }
     prefix[i] = NULL;
     return i;
@@ -278,15 +278,22 @@ create_content_object(struct ccnl_relay_s *ccnl, struct ccnl_prefix_s *prefix,
 }
 
 struct ccnl_content_s *
-ccnl_nfn_local_content_search(struct ccnl_relay_s *ccnl, struct ccnl_interest_s *i){
+ccnl_nfn_local_content_search(struct ccnl_relay_s *ccnl, char **namecomp, int compcnt){
     DEBUGMSG(2, "ccnl_nfn_local_content_search()\n");
-    
-    struct ccnl_content_s *c_iter; 
-    for(c_iter = ccnl->contents; c_iter; c_iter = c_iter->next){
-        unsigned char *md;
-        if(!ccnl_prefix_cmp(c_iter->name, NULL, i->prefix, CMP_EXACT)){
-             i = ccnl_interest_remove(ccnl, i);
-            return c_iter;
+
+    struct ccnl_content_s *content;
+    struct ccnl_prefix_s *prefix = malloc(sizeof(struct ccnl_prefix_s));
+    int i;
+    prefix->comp = (unsigned char**)namecomp;
+    prefix->compcnt = compcnt;
+    prefix->complen = malloc(sizeof(int)*compcnt);
+    for(i = 0; i < compcnt; ++i){
+        prefix->complen[i] = strlen(namecomp[i]);
+    }
+
+    for(content = ccnl->contents; content; content = content->next){
+        if(!ccnl_prefix_cmp(prefix, 0, content->name, CMP_EXACT)){
+            return content;
         }
     }
     return NULL;
@@ -309,84 +316,6 @@ ccnl_extract_content_obj(struct ccnl_relay_s* ccnl, char *out, int len){
 }
 
 struct ccnl_content_s *
-ccnl_receive_content_synchronous(struct ccnl_relay_s *ccnl, struct ccnl_interest_s *interest){
-    int retry = 3;
-    DEBUGMSG(2, "ccnl_receive_content_synchronous()\n");
-    int i, maxfd = -1, rc, content_received = 0;
-    fd_set readfs, writefs;
-    unsigned char buf[CCNL_MAX_PACKET_SIZE];
-    memset(buf, 0, CCNL_MAX_PACKET_SIZE);
-    int len;
-    if (ccnl->ifcount == 0) {
-	fprintf(stderr, "no socket to work with, not good, quitting\n");
-	exit(EXIT_FAILURE);
-    }
-    for (i = 0; i < ccnl->ifcount; i++)
-	if (ccnl->ifs[i].sock > maxfd)
-	    maxfd = ccnl->ifs[i].sock;
-    maxfd++;
-    
-    while(retry){
-        
-        //Initialize sockets
-        struct timeval timeout; //TODO change timeout system, not static
-        timeout.tv_sec = 1;
-        timeout.tv_usec = 0;
-        FD_ZERO(&readfs);
-        FD_ZERO(&writefs);
-        for (i = 0; i < ccnl->ifcount; i++) {
-            FD_SET(ccnl->ifs[i].sock, &readfs);
-            if (ccnl->ifs[i].qlen > 0)
-                FD_SET(ccnl->ifs[i].sock, &writefs);
-        }
-        rc = select(maxfd, &readfs, &writefs, NULL, &timeout);
-        if (rc < 0) {
-            perror("select(): ");
-            exit(EXIT_FAILURE);
-        }
-        //receive content
-        for (i = 0; i < ccnl->ifcount; i++) {
-	    if (FD_ISSET(ccnl->ifs[i].sock, &readfs)) {
-		sockunion src_addr;
-		socklen_t addrlen = sizeof(sockunion);
-		if ((len = recvfrom(ccnl->ifs[i].sock, buf, sizeof(buf), 0,
-				(struct sockaddr*) &src_addr, &addrlen)) > 0) {
-                    //how to handle interest
-                    struct ccnl_content_s *c = ccnl_extract_content_obj(ccnl, buf, len);
-                    unsigned char *md = interest->prefix->compcnt - c->name->compcnt == 1 ? compute_ccnx_digest(c->pkt) : NULL;
-                    if(ccnl_prefix_cmp(c->name, md, interest->prefix, CMP_MATCH)){
-                        return c;
-                    }
-                    else{//else --> normal packet handling???
-                        if (src_addr.sa.sa_family == AF_INET) {
-                                ccnl_core_RX(ccnl, i, buf, len,
-				     &src_addr.sa, sizeof(src_addr.ip4));
-                        }
-#ifdef USE_ETHERNET
-                        else if (src_addr.sa.sa_family == AF_PACKET) {
-                            if (len > 14){
-                                ccnl_core_RX(ccnl, i, buf+14, len-14,
-                                             &src_addr.sa, sizeof(src_addr.eth));
-                            }
-                        }
-#endif
-#ifdef USE_UNIXSOCKET
-                        else if (src_addr.sa.sa_family == AF_UNIX) {
-                            ccnl_core_RX(ccnl, i, buf, len,
-                                         &src_addr.sa, sizeof(src_addr.ux));
-                        }
-#endif
-                    }
-                    
-                }
-            }
-        }
-        --retry;
-    }
-    return NULL;
-}
-
-struct ccnl_content_s *
 ccnl_nfn_global_content_search(struct ccnl_relay_s *ccnl, struct configuration_s *config, 
         struct ccnl_interest_s *interest, int search_only_local){
     struct ccnl_content_s *c;
@@ -394,8 +323,9 @@ ccnl_nfn_global_content_search(struct ccnl_relay_s *ccnl, struct configuration_s
     DEBUGMSG(2, "ccnl_nfn_global_content_search()\n");
     
     //local search. look if content is  available!
-    if((c = ccnl_nfn_local_content_search(ccnl, interest)) != NULL){
+    if((c = ccnl_nfn_local_content_search(ccnl, interest->prefix->comp, interest->prefix->compcnt)) != NULL){
         DEBUGMSG(49, "Content now available: %s\n", c->content);
+        interest = ccnl_interest_remove(ccnl, interest);
         return c;
     }
     if(search_only_local) {
@@ -405,19 +335,6 @@ ccnl_nfn_global_content_search(struct ccnl_relay_s *ccnl, struct configuration_s
     interest->from->faceid = config->configid;
     ccnl_interest_propagate(ccnl, interest);
     return NULL;
-}
-
-//FIXME use global search or special face?
-struct ccnl_content_s *
-ccnl_nfn_content_computation(struct ccnl_relay_s *ccnl, struct ccnl_interest_s *i){
-    DEBUGMSG(2, "ccnl_nfn_content_computation()\n");
-    
-    ccnl_interest_propagate(ccnl, i);
-    //copy receive system to here from core
-    
-    struct ccnl_content_s *c = ccnl_receive_content_synchronous(ccnl, i);
-    
-    return c;
 }
 
 struct ccnl_interest_s *
@@ -440,23 +357,6 @@ ccnl_nfn_create_interest_object(struct ccnl_relay_s *ccnl, struct configuration_
     from->outq->data[0] = strdup(name);
     from->outq->datalen = strlen(name);
     return ccnl_interest_new(ccnl, from, &buf, &p, minsfx, maxsfx, &ppkd);
-}
-
-int
-isLocalAvailable(struct ccnl_relay_s *ccnl, struct configuration_s *config, char **namecomp){
-    DEBUGMSG(2, "isLocalAvailable()\n");
-    char out[CCNL_MAX_PACKET_SIZE];
-    memset(out, 0, CCNL_MAX_PACKET_SIZE);
-    int len = mkInterest(namecomp, 0, out);
-    struct ccnl_interest_s *interest = ccnl_nfn_create_interest_object(ccnl, config, out, len, namecomp[0]);
-    interest->propagate = 0;
-    int found = 0;
-    struct ccnl_content_s *c;
-    if((c = ccnl_nfn_local_content_search(ccnl, interest)) != NULL){ //todo: exact match not only prefix
-        found = 1;
-    }    
-    interest = ccnl_interest_remove(ccnl, interest);
-    return found;
 }
 
 char * 
@@ -540,5 +440,5 @@ ccnl_nfn_resolve_thunk(struct ccnl_relay_s *ccnl, struct configuration_s *config
     }
     return NULL;
 }
-#endif USE_UTIL
+#endif //USE_UTIL
 #endif //KRIVINE_COMMON_C
