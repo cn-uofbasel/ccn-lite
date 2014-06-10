d352026b92a6749f6d4800389747b762f3e0f955
Basil Kohler <basil.kohler@gmail.com>
2014-06-10 17:04:13 +0200

    copied raw ccnl-core.c to make sure fixing conflict did not change anything

--- nfn/src/main/scala/evaluation/Experiment2.scala
+++ nfn/src/main/scala/evaluation/Experiment2.scala
@@ -112,7 +112,7 @@ object Experiment2 extends App {
 
   import LambdaDSL._
   import LambdaNFNImplicits._
-  implicit val useThunks: Boolean = true
+  implicit val useThunks: Boolean = false
 
   val wc = WordCountService().toString
   val ss = SumService().toString
@@ -148,7 +148,7 @@ object Experiment2 extends App {
 
   import AkkaConfig.timeout
   var startTime = System.currentTimeMillis()
-  node1 ? expr onComplete {
+  node1 ? Interest(docname2) onComplete {
     case Success(content) => {
       val totalTime = System.currentTimeMillis - startTime
       println(s"Res($totalTime): $content")
