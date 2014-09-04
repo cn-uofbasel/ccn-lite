147b3373afbf9e50f91c219c20caac7338b1322b
Christopher Scherb <christopher.scherb@unibas.ch>
2014-09-04 15:36:51 +0200

    Extend mkI for ndntlv Bugfix in mkI

--- util/ccn-lite-pktdump.c
+++ util/ccn-lite-pktdump.c
@@ -679,7 +679,7 @@ ndn_parse_sequence(int lev, unsigned char *base, unsigned char **buf,
 }
 
 static void
-ndntlv_201311(unsigned char *data, int len, FILE* out)
+ndntlv_201311(unsigned char *data, int len, int rawxml, FILE* out)
 {
     unsigned char *buf = data;
 
@@ -915,7 +915,7 @@ main(int argc, char *argv[])
 	break;
     case SUITE_NDNTLV:
 	printf("#   %s NDN TLV format (as of Mar 2014)\n#\n", forced);
-	ndntlv_201311(data, len, rawxml);
+	ndntlv_201311(data, len, rawxml, out);
 	break;
     case SUITE_LOCRPC:
 	printf("#   %s NDN TLV format, local RPC (May 2014)\n#\n", forced);
