40b652cb4192211157096c3195c10717b6855b35
Basil Kohler <basil.kohler@gmail.com>
2014-09-05 12:07:04 +0200

    Improved CCNLiteInterface tests, not yet exhaustive

--- nfn-scala/ccnliteinterface/src/test/scala/ccnliteinterface/CCNLiteInterfaceTest.scala
+++ nfn-scala/ccnliteinterface/src/test/scala/ccnliteinterface/CCNLiteInterfaceTest.scala
@@ -19,6 +19,19 @@ class CCNLiteInterfaceTest extends FlatSpec with Matchers with GivenWhenThen {
 
     val ccnIf = CCNLiteInterface.createCCNLiteInterface(wireFormat, ifType)
 
+    def shortenStringToN(str: String, n: Int) =
+      if(str.size > n) {
+        str.take(n) + s" ... (size=${str.size})"
+      } else {
+        str
+      }
+
+    def nameToString(name: Array[String]): String = {
+      name.map { cmp =>
+        shortenStringToN(cmp, 20)
+      } mkString("[ ", " | ", " ]")
+    }
+
     def stringContainsBase64EncodedString(base: String, matcher: String): Boolean = {
       val base64Enc = new BASE64Encoder()
       val b64Cmp = base64Enc.encode(matcher.getBytes)
@@ -26,24 +39,25 @@ class CCNLiteInterfaceTest extends FlatSpec with Matchers with GivenWhenThen {
     }
 
     def testCCNLiteInterfaceCreateInterestWithName(name: Array[String]) = {
-      s"CCNLiteInterface $ifType with wire format $wireFormat for name ${name.mkString("[", ", ", "]")}" should s"convert to an interest msg, back to xml and contain the name" in {
+      s"CCNLiteInterface $ifType with wire format $wireFormat for name ${nameToString(name)}" should s"convert to an interest msg, back to xml and contain the name" in {
         Given(s" $wireFormat for name")
         val wfInterest = ccnIf.mkBinaryInterest(name)
         When("parsed to xml string")
         val xml = ccnIf.ccnbToXml(wfInterest)
-        Then(s"xml:\n$xml\nparsed to interest")
+        Then(s"xml parsed to interest")
         name.foreach { cmp =>
           stringContainsBase64EncodedString(xml, cmp) shouldBe true
         }
       }
     }
     def testCCNLiteInterfaceCreateContent(name: Array[String], dataString: String) = {
-      s"CCNLiteInterface $ifType with wire format $wireFormat for name ${name.mkString("[", ", ", "]")} with data $dataString" should "convert to a content msg, back to xml and contain the name and data" in {
+
+      s"CCNLiteInterface $ifType with wire format $wireFormat for name ${nameToString(name)} with data ${shortenStringToN(dataString, 20)}" should "convert to a content msg, back to xml and contain the name and data" in {
         Given(s"$wireFormat for name and data")
         val wfContent = ccnIf.mkBinaryContent(name, dataString.getBytes)
         When("parsed to xml string")
         val xml = ccnIf.ccnbToXml(wfContent)
-        Then(s"xml:\n$xml\nparsed to content")
+        Then(s"xml: parsed to content")
         name.foreach { cmp =>
           stringContainsBase64EncodedString(xml, cmp) shouldBe true
         }
@@ -51,11 +65,27 @@ class CCNLiteInterfaceTest extends FlatSpec with Matchers with GivenWhenThen {
       }
     }
 
-    val names: List[Array[String]] = List(
-      Array("test", "name"),
-      Array[String](),
-      Array("a"*57)
-    )
+
+
+
+    val names: List[Array[String]] = {
+      def nameCmpOfSizeN(n: Int) = {
+        val cmpDescription = s"cmp${n}chars"
+
+        if (n > cmpDescription.size) {
+          cmpDescription + "a" * (n - cmpDescription.size)
+        } else {
+          cmpDescription.take(n)
+        }
+      }
+
+      List(
+        Array("test", "name"),
+        Array[String](),
+        Array(nameCmpOfSizeN(57)),
+        Array(nameCmpOfSizeN(57), nameCmpOfSizeN(57))
+      )
+    }
 
     val dataStrings: List[String] = List(
       "testdata",
