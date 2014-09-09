54124744f60014ab09716d79fa521351aa636bee
Basil Kohler <basil.kohler@gmail.com>
2014-09-09 13:50:31 +0200

    Removed nfn-scala and nfn-scala-experiments from repository. - nfn-scala moved to its own repository at https://github.com/cn-uofbasel/nfn-scala - the git history is not yet pruned, if the repository gets too big all nfn-scala/* files can be pruned

--- nfn-scala/lambdacalc/src/main/scala/lambdacalculus/machine/CallByName/CBNValue.scala
+++
@@ -1,6 +0,0 @@
-package lambdacalculus.machine.CallByName
-
-import lambdacalculus.machine._
-
-trait CBNValue extends Value
-case class ClosureThunk(c: List[Instruction], e: List[Value], maybeContextName: Option[String] = None) extends CBNValue
