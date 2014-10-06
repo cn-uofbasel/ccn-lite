42b505b2fadc5dfda9a54b5df140e7252bb6e29e
Christian F. Tschudin <christian.tschudin@unibas.ch>
2014-10-06 19:13:06 +0200

    moved Krivine related stuff into ccnl-ext-nfnXXX.c files

--- ccnl-ext-nfncommon.c
+++ ccnl-ext-nfncommon.c
@@ -1,30 +1,135 @@
 /*
- * krivine-common.c
- * Tools for the "Krivine lambda expression resolver" for CCN
+ * @f ccnl-ext-nfncommon.c
+ * @b CCN-lite, tools for the "Krivine lambda expression resolver"
  *
- * (C) 2014 <christian.tschudin@unibas.ch>
+ * Copyright (C) 2014, Christopher Scherb, University of Basel
  *
- * 2014-03-14 created <christopher.scherb@unibas.ch>
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * File history:
+ * 2014-03-14 created 
  */
 
-#ifndef KRIVINE_COMMON_C
-#define KRIVINE_COMMON_C
+// ----------------------------------------------------------------------
 
-#ifndef CCNL_LINUXKERNEL
+struct ccnl_interest_s *
+mkInterestObject(struct ccnl_relay_s *ccnl, struct configuration_s *config,
+                 struct ccnl_prefix_s *prefix)
+{
 
-#include "ccnl-ext-nfn.h"
+    DEBUGMSG(2, "mkInterestObject()\n");
+    int scope=3, aok=3, minsfx=0, maxsfx=CCNL_MAX_NAME_COMP, contlen, mbf=0, len, typ, num, i;
+    struct ccnl_buf_s *buf = 0, *ppkd=0, *nonce=0;
+    struct ccnl_prefix_s *p = 0;
+    unsigned char *out = malloc(CCNL_MAX_PACKET_SIZE);
+    unsigned char *content;
 
-//#include "ccnl.h"
-#ifdef USE_SUITE_CCNB
-#include "pkt-ccnb-enc.c"
-#endif
-#ifdef USE_SUITE_NDNTLV
-#include "pkt-ndntlv-enc.c"
-#endif
 
-#include "ccnl-objects.c"
+    struct ccnl_face_s * from = ccnl_malloc(sizeof(struct ccnl_face_s *));
+    from->faceid = config->configid;
+    from->last_used = CCNL_NOW();
+    from->outq = malloc(sizeof(struct ccnl_buf_s));
+    from->outq->data[0] = (long)strdup((char *)prefix->comp[0]);
+    from->outq->datalen = strlen((char *)prefix->comp[0]);
 
-#include "ccnl-core.h"
+    char *namecomps[CCNL_MAX_NAME_COMP];
+    for(i = 0; i < prefix->compcnt; ++i){
+        namecomps[i] = strdup((char *)prefix->comp[i]);
+    }
+    namecomps[prefix->compcnt] = 0;
+
+    if(config->suite == CCNL_SUITE_CCNB){
+
+        len = ccnl_ccnb_mkInterest(namecomps, NULL, out, 0);
+        if(ccnl_ccnb_dehead(&out, &len, &num, &typ)){
+            return 0;
+        }
+        buf = ccnl_ccnb_extract(&out, &len, &scope, &aok, &minsfx, &maxsfx,
+                                &p, &nonce, &ppkd, &content, &contlen);
+        return ccnl_interest_new(ccnl, from, CCNL_SUITE_CCNB, &buf, &p, minsfx, maxsfx);
+    }
+    else if(config->suite == CCNL_SUITE_CCNTLV){
+        //NOT YET IMPLEMETED
+        return 0;
+    }
+    else if(config->suite == CCNL_SUITE_NDNTLV){
+       int tmplen = CCNL_MAX_PACKET_SIZE;
+       int nonce2;
+       len = ccnl_ndntlv_mkInterest(namecomps, -1, &nonce2, &tmplen, out);
+       memmove(out, out + tmplen, CCNL_MAX_PACKET_SIZE - tmplen);
+       len = CCNL_MAX_PACKET_SIZE - tmplen;
+       unsigned char *cp = out;
+       if(ccnl_ndntlv_dehead(&out, &len, &typ, &num)){
+           return 0;
+       }
+       buf = ccnl_ndntlv_extract(out - cp, &out, &len, &scope, &mbf, &minsfx, &maxsfx,
+                                 &p, &nonce, &ppkd, &content, &contlen);
+       return ccnl_interest_new(ccnl, from, CCNL_SUITE_NDNTLV, &buf, &p, minsfx, maxsfx);
+    }
+    return 0;
+}
+
+struct ccnl_content_s *
+create_content_object(struct ccnl_relay_s *ccnl, struct ccnl_prefix_s *prefix,
+        unsigned char *contentstr, int contentlen, int suite){
+    DEBUGMSG(49, "create_content_object()\n");
+    int i = 0;
+    int scope=3, aok=3, minsfx=0, maxsfx=CCNL_MAX_NAME_COMP, contlen, len, mbf = 0;
+    struct ccnl_buf_s *buf = 0, *nonce=0, *ppkd=0;
+    struct ccnl_prefix_s *p = 0;
+    unsigned char *content = malloc(CCNL_MAX_PACKET_SIZE);
+    int num; int typ;
+    unsigned char *out = malloc(CCNL_MAX_PACKET_SIZE);
+    memset(out, 0, CCNL_MAX_PACKET_SIZE);
+
+    char **prefixcomps = ccnl_malloc(sizeof(char *) * prefix->compcnt+1);
+    for(i = 0; i < prefix->compcnt; ++i)
+    {
+        prefixcomps[i] = strdup((char *)prefix->comp[i]);
+    }
+    prefixcomps[prefix->compcnt] = 0;
+
+    if(suite == CCNL_SUITE_CCNB){
+        len = ccnl_ccnb_mkContent(prefixcomps, (char*)contentstr, contentlen, out);
+        if(ccnl_ccnb_dehead(&out, &len, &num, &typ)){
+            return NULL;
+        }
+        buf = ccnl_ccnb_extract(&out, &len, &scope, &aok, &minsfx, &maxsfx,
+                              &p, &nonce, &ppkd, &content, &contlen);
+        return ccnl_content_new(ccnl, CCNL_SUITE_CCNB, &buf, &p, &ppkd, content, contlen);
+    }
+    else if(suite == CCNL_SUITE_CCNTLV){
+        // net yet implemeted;
+        return 0;
+    }
+    else if(suite == CCNL_SUITE_NDNTLV){
+        int len2 = CCNL_MAX_PACKET_SIZE;
+        len = ccnl_ndntlv_mkContent(prefixcomps, contentstr, contentlen, &len2, out);
+        memmove(out, out+len2, CCNL_MAX_PACKET_SIZE - len2);
+        len = CCNL_MAX_PACKET_SIZE - len2;
+
+        unsigned char *cp = out;
+        if(ccnl_ndntlv_dehead(&out, &len, &typ, &num)){
+            return NULL;
+        }
+        buf = ccnl_ndntlv_extract(out - cp, &out, &len, &scope, &mbf, &minsfx, &maxsfx,
+                                  &p, &nonce, &ppkd, &content, &contlen);
+        return ccnl_content_new(ccnl, CCNL_SUITE_NDNTLV, &buf, &p, &ppkd, content, contlen);
+    }
+    return 0;
+}
+
+// ----------------------------------------------------------------------
 
 struct fox_machine_state_s *
 new_machine_state(int thunk_request, int num_of_required_thunks){
@@ -410,5 +515,6 @@ ccnl_nfn_resolve_thunk(struct ccnl_relay_s *ccnl, struct configuration_s *config
     return NULL;
 }
 #endif //USE_UTIL
-#endif // CCNL_LINUXKERNEL
-#endif //KRIVINE_COMMON_C
+
+
+// eof
