3c634a92534d263e4aa301733aae4345f758dc70
Christian F. Tschudin <christian.tschudin@unibas.ch>
2014-10-14 22:50:11 +0200

    untabyfing a first file (ccnl-ext-nfn.c)

--- ccnl-ext-nfncommon.c
+++ ccnl-ext-nfncommon.c
@@ -565,33 +565,6 @@ fprintf(stderr, "+++ nack->pkt is %p\n", (void*) nack->pkt);
 }
 #endif // USE_NACK
 
-#ifdef OLDCODE
-struct ccnl_interest_s*
-ccnl_interest_remove_continue_computations(struct ccnl_relay_s *ccnl, 
-					   struct ccnl_interest_s *i)
-{
-    struct ccnl_interest_s *interest;
-    int faceid = 0;
-
-    if (i != 0 && i->from != 0)
-            faceid = i->from->faceid;
-    DEBUGMSG(99, "ccnl_interest_remove_continue_computations(faceid=%d)\n", faceid);
-
-#ifdef USE_NACK
-    DEBUGMSG(99, "FROM FACEID: %d\n", faceid);
-    if (faceid >= 0)
-        ccnl_nack_reply(ccnl, i->prefix, i->from, i->suite);
-#endif
-
-    interest = ccnl_interest_remove(ccnl, i);
-
-    if (faceid < 0)
-            ccnl_nfn_continue_computation(ccnl, -i->from->faceid, 1);
-
-    return interest;
-}
-#endif // oldcode
-
 struct ccnl_interest_s*
 ccnl_nfn_interest_remove(struct ccnl_relay_s *relay, struct ccnl_interest_s *i)
 {
