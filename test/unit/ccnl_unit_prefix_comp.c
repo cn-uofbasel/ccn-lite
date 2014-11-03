09235c55a4db6f011f90bd6e8049866f96a278b1
Christopher Scherb <christopher.scherb@unibas.ch>
2014-11-03 16:32:30 +0100

    Add simple unit testing

---
+++ test/unit/ccnl_unit_prefix_comp.c
@@ -0,0 +1,51 @@
+#include "test.h"
+#include "../../ccnl-headers.h"
+
+int ccnl_test_prepare_prefix_cmp_exact_match(void **prefix1, void **prefix2){
+
+	*prefix1 = ccnl_malloc(sizeof(struct ccnl_prefix_s));
+	*prefix2 = ccnl_malloc(sizeof(struct ccnl_prefix_s));
+	struct ccnl_prefix_s *p1 = *prefix1;
+	struct ccnl_prefix_s *p2 = *prefix2;
+
+	p1->compcnt = 3;
+	p1->comp = ccnl_malloc(sizeof(char*) * p1->compcnt);
+	p1->complen = ccnl_malloc(sizeof(int) * p1->compcnt);
+	p1->comp[0] = "path";
+	p1->complen[0] = 4;
+	p1->comp[1] = "to";
+	p1->complen[1] = 2;
+	p1->comp[2] = "data";
+	p1->complen[2] = 4;
+	p1->nfnflags = 0;
+
+	p2->compcnt = 3;
+	p2->comp = ccnl_malloc(sizeof(char*) * p2->compcnt);
+	p2->complen = ccnl_malloc(sizeof(int) * p2->compcnt);
+	p2->comp[0] = "path";
+	p2->complen[0] = 4;
+	p2->comp[1] = "to";
+	p2->complen[1] = 2;
+	p2->comp[2] = "data";
+	p2->complen[2] = 4;
+	p2->nfnflags = 0;
+	return 1;
+}
+
+int ccnl_test_run_prefix_cmp_exact_match(void *prefix1, void *prefix2){
+
+	struct ccnl_prefix_s *p1 = prefix1;
+	struct ccnl_prefix_s *p2 = prefix2;
+
+	int res = ccnl_prefix_cmp(prefix1, 0, prefix2, CMP_EXACT);
+	//NULL if match
+	return !res;
+}
+
+int ccnl_test_cleanup_prefix_cmp_exact_match(void *prefix1, void *prefix2){
+
+	return 1;
+}
+
+//#undef CCNL_EXT_DEBUG
+//#undef USE_DEBUG_MALLOC
---
+++ test/unit/test.h
@@ -0,0 +1,76 @@
+#ifndef CCNL_UNIT_TEST_H
+#define CCNL_UNIT_TEST_H
+
+
+#define USE_DEBUG
+#define USE_DEBUG_MALLOC
+#define CCNL_UNIX
+#define USE_NFN
+#define USE_NFN_MONITOR
+
+//#define ccnl_core_addToCleanup(b)	do{}while(0)
+
+enum {STAT_RCV_I, STAT_RCV_C, STAT_SND_I, STAT_SND_C, STAT_QLEN, STAT_EOP1};
+
+
+#define ccnl_frag_destroy(e)            do{}while(0)
+#define ccnl_print_stats(x,y)           do{}while(0)
+#define ccnl_app_RX(x,y)		do{}while(0)
+
+#include "unit.h"
+
+#include <dirent.h>
+#include <fnmatch.h>
+#include <regex.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+#define CCNL_UNIX
+
+#define USE_CCNxDIGEST
+#define USE_DEBUG                      // must select this for USE_MGMT
+#define USE_DEBUG_MALLOC
+#define USE_ETHERNET
+#define USE_HTTP_STATUS
+#define USE_NACK
+#define USE_NFN
+#define USE_NFN_NSTRANS
+#define USE_SUITE_CCNB                 // must select this for USE_MGMT
+#define USE_SUITE_CCNTLV
+#define USE_SUITE_NDNTLV
+#define USE_SUITE_LOCALRPC
+#define USE_UNIXSOCKET
+
+#include "../../ccnl-includes.h"
+
+#include "../../ccnl-headers.h"
+
+#include "../../ccnl.h"
+#include "../../ccnl-core.h"
+
+
+#include "../../ccnl-ext-debug.c"
+#include "../../ccnl-ext.h"
+#include "../../ccnl-platform.c"
+
+#define ccnl_app_RX(x,y)                do{}while(0)
+#define ccnl_print_stats(x,y)           do{}while(0)
+#define ccnl_close_socket(a)		do{}while(0)
+#define ccnl_ll_TX(a,b,c,d)		do{a=a;}while(0)
+
+#include "../../ccnl-core.c"
+#include "../../ccnl-util.c"
+
+#include "../../ccnl-ext-http.c"
+#include "../../ccnl-ext-mgmt.c"
+#include "../../ccnl-ext-nfn.c"
+#include "../../ccnl-ext-nfnmonitor.c"
+#include "../../ccnl-ext-sched.c"
+#include "../../ccnl-ext-frag.c"
+#ifdef USE_SIGNATURES
+#include "../../ccnl-ext-crypto.c"
+#endif
+
+
+#endif
