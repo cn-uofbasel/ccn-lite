9f73eadf1a337ecea9b004daad6380536d2c5114
Christopher Scherb <christopher.scherb@unibas.ch>
2014-12-15 11:38:24 +0100

    Enable DEBUGMSG for util Add logging file

--- src/util/ccn-lite-fetch.c
+++ src/util/ccn-lite-fetch.c
@@ -161,7 +161,7 @@ ccnl_prefix_removeChunkNumComponent(int suite,
         if(prefix->comp[prefix->compcnt-1][1] == CCNX_TLV_N_Chunk) {
             prefix->compcnt--;
         } else {
-            DEBUGMSG(WARN, "Tried to remove chunknum from CCNTLV prefix, but either prefix does not have a chunknum "
+            DEBUGMSG(WARNING, "Tried to remove chunknum from CCNTLV prefix, but either prefix does not have a chunknum "
                            "or the last component is not the chunknum.");
             return -1;
         }
