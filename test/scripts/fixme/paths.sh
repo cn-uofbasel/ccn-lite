5fd951803283e454983fbd8f4771230cf4ef7291
Christian F. Tschudin <cft@l2apps.com>
2014-12-23 14:48:18 +0100

    merged from dev-master, now we are almost ready for release 0.2.0

--- test/scripts/fixme/paths.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# ccn-lite/test/scripts/paths.sh
-
-#CCND_HOME=~/ccn-lite/ccnx-0.7.1
-CCND_HOME=~/ccn-lite/ccnx-0.8.0
-CCNL_HOME=~/ccn-lite/ccn-lite
-
-# Use XEROX PUP as ethernet type:
-CCNL_ETH_TYPE=0x0a00
