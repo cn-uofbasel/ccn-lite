3d367962dfca7212a013f2b3b6d5872bfce2128a
Lukas Beck <lukas.beck@gmx.com>
2015-07-01 18:24:54 +0200

    Improved formatting and output of nfn test script.

--- test/scripts/nfn/nfn_test.sh
+++ test/scripts/nfn/nfn_test.sh
@@ -14,7 +14,7 @@ echo "Testing NFN with a sample computation. Two NFN nodes, one is connected to 
 
 for PACKETTYPE in $PACKETTYPELIST; do
 
-     printf '  Testing Packet Format %s [..]' $PACKETTYPE
+    printf '    Testing Packet Format %-10s [..]' $PACKETTYPE
 
     #echo '** killing all CCNL relay and Java instances'
 
@@ -25,7 +25,7 @@ for PACKETTYPE in $PACKETTYPELIST; do
     sleep 2
 
 
-    printf '\b\b\bStarting two CCNL relays]'
+    printf '\b\b\bStarting two CCNL relays..]'
     #echo '** starting two CCNL relays'
     "$CCNL_HOME/bin/ccn-nfn-relay" -v trace -u 9000 -x /tmp/mgmt1.sock 2> "/tmp/$PACKETTYPE-relay0.log" &
     "$CCNL_HOME/bin/ccn-nfn-relay" -v trace -u 9001 -x /tmp/mgmt2.sock 2> "/tmp/$PACKETTYPE-relay1.log" &
@@ -34,33 +34,33 @@ for PACKETTYPE in $PACKETTYPELIST; do
     #echo '** in 3 seconds, adding an UDP interface, twice'
     sleep 3
 
-    printf '\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bAdding an UDP interface]  \b\b'
+    printf '\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bAdding an UDP interface..]  \b\b'
     "$CCNL_HOME/bin/ccn-lite-ctrl" -x /tmp/mgmt1.sock newUDPface any 127.0.0.1 9001  | "$CCNL_HOME/bin/ccn-lite-ccnb2xml" > "/tmp/$PACKETTYPE-command0.log"
 
     #echo '** in 3 seconds, registering a prefix, twice'
     sleep 3
 
-    printf '\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bRegistering a prefix]    \b\b\b\b'
+    printf '\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bRegistering a prefix..]    \b\b\b\b'
     "$CCNL_HOME/bin/ccn-lite-ctrl" -x /tmp/mgmt1.sock prefixreg /nfn/node2 2 "$PACKETTYPE" | "$CCNL_HOME/bin/ccn-lite-ccnb2xml" > "/tmp/$PACKETTYPE-command1.log"
 
     #echo '** in 3 seconds, starting the NFN compute server'
     sleep 3
-    
 
-    printf '\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bStarting the NFN compute server]    \b\b\b\b'
+
+    printf '\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\bStarting the NFN compute server..]    \b\b\b\b'
     java -jar "$CCNL_HOME/test/scripts/nfn/nfn.jar" -s "$PACKETTYPE" -m /tmp/mgmt2.sock -o 9001 -p 9002 -d -r /nfn/node2 2>&1 >  "/tmp/$PACKETTYPE-computserver.log" &
 
     #echo '** in 8 seconds, probing the NFN system'
     sleep 8
 
-    RES=`ccn-lite-simplenfn -s "$PACKETTYPE" -u '127.0.0.1/9000' 'call 2 /nfn/node2/nfn_service_WordCount /nfn/node2/docs/tutorial_md' | ccn-lite-pktdump -f 2`
+    RES=`ccn-lite-simplenfn -s "$PACKETTYPE" -u '127.0.0.1/9000' 'call 2 /nfn/node2/nfn_service_WordCount /nfn/node2/docs/tutorial_md' 2> /dev/null | ccn-lite-pktdump -f 2`
     RC=$?
     #echo "Return code: $RC, result: '$RES'"
 
     if [ 0 -ne "$RC" ] || [ 3744 -ne "$RES" ]; then
-         echo $'\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\e[91mFAILED\e[0m]                              \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b'
+         echo $'\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\e[1;31mFAILED\e[0;0m]                                \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b'
     else
-         echo $'\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\e[92mOK\e[0m]                              \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b'
+         echo $'\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\e[1;32mOK\e[0;0m]                                \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b'
     fi
 
     "$CCNL_HOME/bin/ccn-lite-ctrl" -x /tmp/mgmt1.sock debug halt | "$CCNL_HOME/bin/ccn-lite-ccnb2xml" > /dev/null
