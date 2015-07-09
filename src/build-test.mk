64cba4f386307e6b9a070bf128d61fa794e03a68
Lukas Beck <lukas.beck@gmx.com>
2015-07-09 09:24:19 +0200

    Added verbose output setting to build-test.

--- src/build-test.mk
+++ src/build-test.mk
@@ -10,7 +10,13 @@ else
 endif
 export NO_CORES
 
-BT_RELAY=bt-relay-nothing \
+ifdef VERBOSE
+    PRINT_LOG=( echo "Contents of /tmp/$@.log:"; cat "/tmp/$@.log"; echo; exit 1 )
+else
+    PRINT_LOG=( exit 1 )
+endif
+
+BT_RELAY:=bt-relay-nothing \
 	bt-relay-barebones \
 	bt-relay-vanilla \
 	bt-relay-frag \
@@ -19,15 +25,15 @@ BT_RELAY=bt-relay-nothing \
 	bt-relay-nack \
 	bt-relay-nfn-nack \
 	bt-relay-all
-BT_LNXKERNEL=bt-lnxkernel
-BT_ALL=bt-all-vanilla \
+BT_LNXKERNEL:=bt-lnxkernel
+BT_ALL:=bt-all-vanilla \
 	bt-all-nfn
-BT_PKT=bt-pkt-ccnb \
+BT_PKT:=bt-pkt-ccnb \
 	bt-pkt-ccntlv \
 	bt-pkt-cistlv \
 	bt-pkt-iottlv \
 	bt-pkt-ndntlv
-PROFILES=${BT_RELAY} ${BT_LNXKERNEL} ${BT_ALL} ${BT_PKT}
+PROFILES:=${BT_RELAY} ${BT_LNXKERNEL} ${BT_ALL} ${BT_PKT}
 
 .PHONY: all clean bt-relay bt-all bt-pkt ${PROFILES}
 all: echo-cores ${PROFILES} clean
@@ -54,7 +60,7 @@ bt-relay-nothing:
 		USE_NACK USE_NFN USE_NFN_NSTRANS USE_NFN_MONITOR USE_SCHEDULER \
 		USE_STATS USE_SUITE_CCNB USE_SUITE_CCNTLV USE_SUITE_CISTLV \
 		USE_SUITE_IOTTLV USE_SUITE_NDNTLV USE_SUITE_LOCALRPC USE_UNIXSOCKET" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-relay-barebones:
 # Build only with USE_IPV4 and USE_SUITE_NDNTLV
@@ -68,102 +74,102 @@ bt-relay-barebones:
 		USE_NACK USE_NFN USE_NFN_NSTRANS USE_NFN_MONITOR USE_SCHEDULER \
 		USE_STATS USE_SUITE_CCNB USE_SUITE_CCNTLV USE_SUITE_CISTLV \
 		USE_SUITE_IOTTLV USE_SUITE_LOCALRPC USE_UNIXSOCKET" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-relay-vanilla:
 	@MODE="make" \
 	TARGET=$@ \
 	MAKE_TARGETS="ccn-lite-relay" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-relay-frag:
 	@MODE="make" \
 	TARGET=$@ \
 	MAKE_TARGETS="ccn-lite-relay" \
 	MAKE_VARS="USE_FRAG=1" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-relay-authCtrl:
 	@MODE="make" \
 	TARGET=$@ \
 	MAKE_TARGETS="ccn-lite-relay" \
 	MAKE_VARS="USE_SIGNATURES=1" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-relay-nfn:
 	@MODE="make" \
 	TARGET=$@ \
 	MAKE_TARGETS="ccn-nfn-relay" \
 	MAKE_VARS="USE_NFN=1" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-relay-nack:
 	@MODE="make" \
 	TARGET=$@ \
 	MAKE_TARGETS="ccn-lite-relay-nack" \
 	MAKE_VARS="USE_NFN=1 USE_NACK=1" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-relay-nfn-nack:
 	@MODE="make" \
 	TARGET=$@ \
 	MAKE_TARGETS="ccn-nfn-relay-nack" \
 	MAKE_VARS="USE_NFN=1 USE_NACK=1" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-relay-all:
 	@MODE="make" \
 	TARGET=$@ \
 	MAKE_TARGETS="ccn-lite-relay" \
 	MAKE_VARS="USE_FRAG=1 USE_SIGNATURES=1" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-lnxkernel:
 	@MODE="make" \
 	TARGET=$@ \
 	MAKE_TARGETS="ccn-lite-lnxkernel" \
 	MAKE_VARS="USE_KRNL=1" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-all-vanilla:
 	@MODE="make" \
 	TARGET=$@ \
 	MAKE_TARGETS="all" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-all-nfn:
 	@MODE="make" \
 	TARGET=$@ \
 	MAKE_TARGETS="all" \
 	MAKE_VARS="USE_NFN=1" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-pkt-ccnb:
 	@MODE="pkt-format" \
 	TARGET=$@ \
 	PKT_FORMAT="ccnb" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-pkt-ccntlv:
 	@MODE="pkt-format" \
 	TARGET=$@ \
 	PKT_FORMAT="ccntlv" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-pkt-cistlv:
 	@MODE="pkt-format" \
 	TARGET=$@ \
 	PKT_FORMAT="cistlv" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-pkt-iottlv:
 	@MODE="pkt-format" \
 	TARGET=$@ \
 	PKT_FORMAT="iottlv" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
 
 bt-pkt-ndntlv:
 	@MODE="pkt-format" \
 	TARGET=$@ \
 	PKT_FORMAT="ndntlv" \
-	./build-test-helper.sh
+	./build-test-helper.sh || ${PRINT_LOG}
