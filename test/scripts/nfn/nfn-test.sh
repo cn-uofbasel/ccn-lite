304ba924be9f6f9b8aa41d395aadaec1ce873fbb
Lukas Beck <lukas.beck@gmx.com>
2015-07-15 12:08:24 +0200

    Made nfn-test killing only the processes it spawns.

--- test/scripts/nfn/nfn-test.sh
+++ test/scripts/nfn/nfn-test.sh
@@ -4,6 +4,9 @@
 # Environment variables
 SUITES=("ccnb" "ccnx2015" "cisco2015" "iot2014" "ndn2013")
 USAGE="usage: nfn-test.sh [-v] SUITE \nwhere\n  SUITE =   ${SUITES[@]}\n  -v        enable verbose output"
+PID1=-1
+PID2=-1
+PID3=-1
 RC=0
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 VERBOSE=0
@@ -41,33 +44,29 @@ NFN_JAR="$CCNL_HOME/test/scripts/nfn/nfn.jar"
 check-nfn "$NFN_JAR"
 
 # Start test
-echo -n "$(date "+[%F %T]") Killing all CCN-lite relay and Java instances..."
-killall ccn-lite-relay 2> /dev/null
-killall ccn-nfn-relay 2> /dev/null
-killall java 2> /dev/null
-sleep 3
-echo " Done!"
-
 echo -n "$(date "+[%F %T]") Starting two CCN-lite relays..."
 "$CCNL_HOME/bin/ccn-nfn-relay" -v trace -u 9000 -x /tmp/mgmt1.sock &> "/tmp/nfn-test-$SUITE-relay0.log" &
+PID1=$!
 "$CCNL_HOME/bin/ccn-nfn-relay" -v trace -u 9001 -x /tmp/mgmt2.sock &> "/tmp/nfn-test-$SUITE-relay1.log" &
+PID2=$!
 sleep 3
 echo " Done!"
 
 echo "$(date "+[%F %T]") Adding UPD interface..."
 echo "$(date "+[%F %T]") $ ccn-lite-ctrl newUDPface any 127.0.0.1 9001 | ccn-lite-ccnb2xml"
-"$CCNL_HOME/bin/ccn-lite-ctrl" -x /tmp/mgmt1.sock newUDPface any 127.0.0.1 9001  | "$CCNL_HOME/bin/ccn-lite-ccnb2xml"
+FACEID=$("$CCNL_HOME/bin/ccn-lite-ctrl" -x /tmp/mgmt1.sock newUDPface any 127.0.0.1 9001 | "$CCNL_HOME/bin/ccn-lite-ccnb2xml" | grep FACEID | sed -e 's/^[^0-9]*\([0-9]\+\).*/\1/')
 sleep 3
 echo " Done!"
 
 echo "$(date "+[%F %T]") Registering prefix..."
-echo "$(date "+[%F %T]") $ ccn-lite-ctrl prefixreg /nfn/node2 2 $SUITE | ccn-lite-ccnb2xml"
-"$CCNL_HOME/bin/ccn-lite-ctrl" -x /tmp/mgmt1.sock prefixreg /nfn/node2 2 "$SUITE" | "$CCNL_HOME/bin/ccn-lite-ccnb2xml"
+echo "$(date "+[%F %T]") $ ccn-lite-ctrl prefixreg /nfn/node2 $FACEID $SUITE | ccn-lite-ccnb2xml"
+"$CCNL_HOME/bin/ccn-lite-ctrl" -x /tmp/mgmt1.sock prefixreg /nfn/node2 "$FACEID" "$SUITE" | "$CCNL_HOME/bin/ccn-lite-ccnb2xml"
 sleep 3
 echo " Done!"
 
 echo -n "$(date "+[%F %T]") Starting NFN compute server..."
 java -jar "$NFN_JAR" -s "$SUITE" -m /tmp/mgmt2.sock -o 9001 -p 9002 -d -r /nfn/node2 &> "/tmp/nfn-test-$SUITE-computserver.log" &
+PID3=$!
 sleep 8
 echo " Done!"
 
@@ -80,8 +79,19 @@ if [[ 0 -ne $RC ]] || [[ 3558 -ne $RES ]]; then
 fi
 
 echo "$(date "+[%F %T]") $RES"
+
+# Stop relays and NFN
 "$CCNL_HOME/bin/ccn-lite-ctrl" -x /tmp/mgmt1.sock debug halt | "$CCNL_HOME/bin/ccn-lite-ccnb2xml" &> /dev/null
 "$CCNL_HOME/bin/ccn-lite-ctrl" -x /tmp/mgmt2.sock debug halt | "$CCNL_HOME/bin/ccn-lite-ccnb2xml" &> /dev/null
+if ps -p $PID1 > /dev/null; then
+    kill $PID1 2> /dev/null
+fi
+if ps -p $PID2 > /dev/null; then
+    kill $PID2 2> /dev/null
+fi
+if ps -p $PID3 > /dev/null; then
+    kill $PID3 2> /dev/null
+fi
 
 # Verbose output
 if [[ 0 -ne $VERBOSE ]] && [[ 0 -ne $RC ]]; then
@@ -100,9 +110,5 @@ else
     echo $' [\e[1;32mok\e[0;0m]'
 fi
 echo "$(date "+[%F %T]") See /tmp/nfn-test-$SUITE-*.log for more information."
-
-killall ccn-lite-relay 2> /dev/null
-killall ccn-nfn-relay  2> /dev/null
-killall java  2> /dev/null
 
 exit $RC
