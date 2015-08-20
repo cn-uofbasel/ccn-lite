2aea1313ba1165a2149988cc1ae936706d51e149
Lukas Beck <lukas.beck@gmx.com>
2015-08-20 15:39:39 +0200

    Fixed parameters of ccnl_content_new call.

--- src/ccnl-ext-crypto.c
+++ src/ccnl-ext-crypto.c
@@ -476,8 +476,7 @@ ccnl_mgmt_crypto(struct ccnl_relay_s *ccnl, char *type, unsigned char *buf, int 
           prefix_a->complen = (int *) ccnl_malloc(sizeof(int)*2);
           prefix_a->complen[0] = strlen("mgmt");
           prefix_a->complen[1] = strlen(ht);
-          c = ccnl_content_new(ccnl, CCNL_SUITE_CCNB, &pkt, &prefix_a, &ppkd,
-                                content, contlen);
+          c = ccnl_content_new(ccnl, &pkt);
           if (!c) goto Done;
 
           ccnl_content_serve_pending(ccnl, c);
