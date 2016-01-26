572b973125921008475c805c99c9d1b8d0a4845c
Oleg Hahm <oleg@hobbykeller.org>
2016-01-26 16:12:21 +0100

    also free chunknum in free_prefix

--- src/ccnl-ext-debug.c
+++ src/ccnl-ext-debug.c
@@ -777,7 +777,7 @@ ccnl_buf_new(void *data, int len)
 #define free_5ptr_list(a,b,c,d,e) ccnl_free(a), ccnl_free(b), ccnl_free(c), ccnl_free(d), ccnl_free(e);
 
 #define free_prefix(p)  do{ if(!p) break; ccnl_free(p->chunknum);  \
-                free_4ptr_list(p->bytes,p->comp,p->complen,p); } while(0)
+                free_5ptr_list(p->bytes,p->comp,p->complen,p->chunknum,p); } while(0)
 #define free_content(c) do{ /* free_prefix(c->name); */ free_packet(c->pkt); \
                         ccnl_free(c); } while(0)
 
