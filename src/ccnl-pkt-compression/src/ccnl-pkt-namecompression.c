fed97c32fbbbfb70103ce76f307389927b0fca11
DMI-Scherb Christopher <christopher.scherb@unibas.ch>
2017-08-11 15:11:32 +0200

    fix memleak

--- src/ccnl-pkt-compression/src/ccnl-pkt-namecompression.c
+++ src/ccnl-pkt-compression/src/ccnl-pkt-namecompression.c
@@ -177,7 +177,7 @@ ccnl_pkt_prefix_decompress(struct ccnl_prefix_s *pfx){
                               decompressed_name, out_len);
     DEBUGMSG(DEBUG, "Extracted Name: %.*s\n", out_len ,(char*)decompressed_name);
     struct ccnl_prefix_s *ret = ccnl_URItoPrefix((char *)decompressed_name, pfx->suite, NULL, NULL);
-    //ccnl_free(ret->bytes);
+    ccnl_free(decompressed_name);
     //ret->bytes = decompressed_name;
     return ret;
 }
