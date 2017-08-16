bf5f2bc6bc342c1af98d9291d95f4db6ce89ad4f
DMI-Scherb Christopher <christopher.scherb@unibas.ch>
2017-08-16 16:52:41 +0200

    bugfix

--- src/ccnl-pkt-compression/src/ccnl-pkt-namecompression.c
+++ src/ccnl-pkt-compression/src/ccnl-pkt-namecompression.c
@@ -170,11 +170,11 @@ ccnl_pkt_prefix_compress(struct ccnl_prefix_s *pfx){
     char name[PREFIX_BUFSIZE];
     ccnl_prefix_to_str(pfx, name, PREFIX_BUFSIZE);
     int name_len = (int)strlen((char* )name);
-    unsigned char *compressed_name = ccnl_malloc(name_len); //must be manually freed(!) since normally prefix->comp points to bin!
+    unsigned char *compressed_name = ccnl_malloc(name_len+1); //must be manually freed(!) since normally prefix->comp points to bin!
     if(!compressed_name){
         return NULL;
     }
-    int compressed_len = ccnl_pkt_compression_str2bytes((unsigned char*)name, 6, compressed_name, name_len);
+    int compressed_len = ccnl_pkt_compression_str2bytes((unsigned char*)name, 6, compressed_name, name_len+1);
     compressed_name = ccnl_realloc(compressed_name, compressed_len+1);
     //create compressed prefix
     struct ccnl_prefix_s *ret = ccnl_prefix_new(pfx->suite, 1);
